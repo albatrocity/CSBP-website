57ef7cbdea0b842dc39571c48cec67694706793c
Rob LaFeve <rob@cremalab.com>
2012-05-25 14:32:31 -0500

    Schedule/Content

---
+++ source/js/functions.js
@@ -0,0 +1,22 @@
+// $(function() {
+	
+// 	$(".band").each(function(){
+// 		var h = $(this).height();
+// 		$(this).attr('data-height', h);
+// 		$(this).height('40');
+// 	});
+
+// 	$( ".name-time" ).click(function(e) {
+// 		e.preventDefault();
+// 		var band = $( this ).parent(".band");
+// 		var h = band.data('height');
+// 		console.log(h);
+// 		$( ".band" ).not(band).animate({
+// 			height: '40'
+// 		});
+// 		band.animate({
+// 			height: 'auto'
+// 		});;
+// 	});
+
+// });
