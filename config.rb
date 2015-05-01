8eeb94b35d5322a4c1f8aa922bad6c8e37dca890
Ross Brown <ross@cremalab.com>
2015-05-01 00:47:29 -0500

    removing build instructions

--- config.rb
+++ config.rb
@@ -70,6 +70,8 @@ sprockets.import_asset 'fadeshow'
 # Change the JS directory
 set :js_dir, "js"
 
+set :build_dir, "tmp"
+
 
 # Change the images directory
 set :images_dir, "img"
@@ -77,23 +79,23 @@ set :images_dir, "img"
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
-  # Minify Javascript on build
-  activate :minify_javascript
+  # # Minify Javascript on build
+  # activate :minify_javascript
 
-  # Enable cache buster
-  # activate :cache_buster
+  # # Enable cache buster
+  # # activate :cache_buster
 
-  # Use relative URLs
-  activate :relative_assets
+  # # Use relative URLs
+  # activate :relative_assets
 
-  activate :directory_indexes
+  # activate :directory_indexes
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  require "middleman-smusher"
-  activate :smusher
+  # require "middleman-smusher"
+  # activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
