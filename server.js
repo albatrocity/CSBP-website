9905494377813c14ec7295d8e56b452d48555595
Ross Brown <ross@cremalab.com>
2017-03-07 20:48:28 -0600

    adding sponsors to header

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ keystone.set('routes', require('./routes'))
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
   users: 'users',
-  pages: 'pages'
+  pages: 'pages',
+  sponsors: 'sponsors'
 })
 
 // Start Keystone to connect to your database and initialise the web server
