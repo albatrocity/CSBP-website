7b239d1aafab62f4a2559364a1811f3df774d24c
Ross Brown <ross@cremalab.com>
2017-03-08 11:07:46 -0600

    fixing sponsors on pages

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,5 +1,9 @@
 const keystone = require('keystone')
+const Sponsor  = keystone.list('Sponsor')
 const Page = keystone.list('Page')
+
+const logoLevels = [ 'Bear Hug', 'Partner']
+const textLevels = [ 'High Five', 'Fist Bump', 'Special']
 
 exports = module.exports = function (req, res) {
   const view = new keystone.View(req, res)
@@ -12,6 +16,16 @@ exports = module.exports = function (req, res) {
       locals.page = page
       locals.title = page.name
       locals.section = page.slug
+      return locals
+    })
+    .then(() => Sponsor.model.find({ visible: true }))
+    .then((sponsors) => {
+      locals.sponsors = {
+        logo: sponsors.filter(x => logoLevels.indexOf(x.tier) > -1)
+          .sort((a, b) => a.sortOrder - b.sortOrder),
+        text: sponsors.filter(x => textLevels.indexOf(x.tier) > -1)
+          .sort((a, b) => a.sortOrder - b.sortOrder)
+      }
       next()
     })
   })
